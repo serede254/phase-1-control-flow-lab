5dd00985f8f32403cfd1492d11bf752b4cbdc5e9
root <root@SURFACE.>
2024-03-19 11:08:11 +0300

    conditionals

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number){
+if (number<=400){
+  return "This one is on me!";
+}
+else if (number > 400 && number < 2000){
+  return "That will be twenty bucks.";
+}
+else if (number > 2000 && number < 2500){
+  return "I will gladly take your thirty bucks.";
+}
+else if(number > 2500){
+  return "No can do.";
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === "NYC") {
+    return "Ok, sounds good.";
+}
+else{
+    return "No go.";
+}
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case tip="generous":
+      return "Thank you so much.";
+    case tip="not as generous":
+      return "Thank you.";
+    default:
+     return "Bye.";
+  }
 }
